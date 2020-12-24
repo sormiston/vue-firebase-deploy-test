521a3e9e4ac429fe9bdf3c1aa6feb1f3bb7396c3
SOrmiston <sean.david.ormiston@gmail.com>
2020-12-23 23:50:27 -0500

    first commit - end lecture 4

---
+++ notes.md
@@ -0,0 +1,12 @@
+## VUE FORMS
+### Max Schwazmueuller Complete Vue Section 12
+<br/>
+
+4. Sending a POST Request to Store Data
+    * Re-route submitted surveys with a POST request to firebase in LearningSurvey component
+    * Comment out all local app-level storage of submitted Surveys
+    * App-level will no longer pass results props to User Experience component (comment that out too)
+    * Test HTTP result.  Remember, firebase should be addressed at slug /surveys.json and the req body must be a JSON object.  Check if a surveys node was annexed to the firebase database tree.
+    
+5. GET Request & Transforming Response
+   
---
+++ src/components/survey/LearningSurvey.vue
@@ -0,0 +1,113 @@
+<template>
+  <section>
+    <base-card>
+      <h2>How was you learning experience?</h2>
+      <form @submit.prevent="submitSurvey">
+        <div class="form-control">
+          <label for="name">Your Name</label>
+          <input type="text" id="name" name="name" v-model.trim="enteredName" />
+        </div>
+        <h3>My learning experience was ...</h3>
+        <div class="form-control">
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
+          <label for="rating-poor">Poor</label>
+        </div>
+        <div class="form-control">
+          <input
+            type="radio"
+            id="rating-average"
+            value="average"
+            name="rating"
+            v-model="chosenRating"
+          />
+          <label for="rating-average">Average</label>
+        </div>
+        <div class="form-control">
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
+          <label for="rating-great">Great</label>
+        </div>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
+        <div>
+          <base-button>Submit</base-button>
+        </div>
+      </form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+const axios = require('axios').default;
+axios.defaults.baseURL =
+  'https://http-vue-demo-a3588-default-rtdb.firebaseio.com/';
+
+export default {
+  data() {
+    return {
+      enteredName: '',
+      chosenRating: null,
+      invalidInput: false,
+    };
+  },
+
+  methods: {
+    submitSurvey() {
+      if (this.enteredName === '' || !this.chosenRating) {
+        this.invalidInput = true;
+        return;
+      }
+      this.invalidInput = false;
+
+      axios
+        .post('/surveys.json', {
+          name: this.enteredName,
+          rating: this.chosenRating,
+        })
+        .then((response) => console.log(response))
+        .catch((error) => {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+            console.log('request error, above!');
+          }
+        });
+
+      this.enteredName = '';
+      this.chosenRating = null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+input[type='text'] {
+  display: block;
+  width: 20rem;
+  margin-top: 0.5rem;
+}
+</style>
